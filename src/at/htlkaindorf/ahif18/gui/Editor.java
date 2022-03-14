2a4d62da58b5066c1e8fe1d23338e4a37dfd0880
Reapie <mjuritsch0405@gmail.com>
2022-03-14 11:41:05 +0100

    Created simple GUI
    Also added out fold to gitignore

--- src/at/htlkaindorf/ahif18/gui/Editor.java
+++ src/at/htlkaindorf/ahif18/gui/Editor.java
@@ -1,4 +1,20 @@
 package at.htlkaindorf.ahif18.gui;
 
+import javax.swing.*;
+
 public class Editor {
+
+    private JPanel mainPanel;
+    private JEditorPane editor;
+    private JEditorPane console;
+    private JButton btnFile;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Editor");
+        frame.setContentPane(new Editor().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
