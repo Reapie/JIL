8d515b13bea0fb9a038ef80f17e4c55b341fa9c9
Rluffy <fabi.ladenhaufen@gmail.com>
2022-04-04 11:40:23 +0200

    Value Funktionen in BinaryExpr verschoben(ADD,SUB,MUL)

--- src/main/java/at/htlkaindorf/ahif18/ast/nodes/BinaryExpr.java
+++ src/main/java/at/htlkaindorf/ahif18/ast/nodes/BinaryExpr.java
@@ -22,13 +22,13 @@ public class BinaryExpr extends Expr {
         try {
             switch (type.getType()) {
                 case TK_PLUS -> {
-                    return left.eval().add(right.eval());
+                    return add(right.eval());
                 }
                 case TK_MINUS -> {
-                    return left.eval().sub(right.eval());
+                    return sub(right.eval());
                 }
                 case TK_MUL -> {
-                    return left.eval().mul(right.eval());
+                    return mul(right.eval());
                 }
                 case TK_DIV -> {
                     return left.eval().div(right.eval());
@@ -44,6 +44,68 @@ public class BinaryExpr extends Expr {
             e.printStackTrace();
             return new Value();
         }
+
+
+    }
+
+    private Value add(Value other) throws ParserException {
+
+        Value.TYPES type = left.eval().getType();
+        String strValue = left.eval().getStrValue();
+        double numValue = left.eval().getNumValue();
+
+        if (type == other.getType()) {
+            if (type == Value.TYPES.NUMBER) {
+                numValue+= right.eval().getNumValue();
+                return new Value(numValue);
+            } else if (type == Value.TYPES.STRING) {
+                strValue = strValue.concat(other.getStrValue());
+                return new Value(strValue);
+            }
+        }
+        return new Value();
+    }
+
+    private Value sub(Value other) throws ParserException {
+        Value.TYPES type = left.eval().getType();
+        String strValue = left.eval().getStrValue();
+        double numValue = left.eval().getNumValue();
+
+        if (type == other.getType()) {
+            if (type == Value.TYPES.NUMBER) {
+                numValue -= other.getNumValue();
+                return new Value(numValue);
+            }
+        }
+        return new Value();
+    }
+
+    private Value mul(Value other) throws ParserException {
+
+        Value.TYPES type = left.eval().getType();
+        String strValue = left.eval().getStrValue();
+        double numValue = left.eval().getNumValue();
+
+        if (type == other.getType()) {
+            if (type == Value.TYPES.NUMBER) {
+                numValue *= other.getNumValue();
+                return new Value(numValue);
+            }
+            // not very pretty but it works :)
+        } else if (type == Value.TYPES.STRING && other.getType() == Value.TYPES.NUMBER) {
+            double num = other.getNumValue();
+            if (num < 0) {other.setNumValue(num * -1);}
+            strValue = new String(new char[(int) other.getNumValue()]).replace("\0", strValue);
+
+            return new Value(strValue);
+        } else if (type == Value.TYPES.NUMBER && other.getType() == Value.TYPES.STRING) {
+            double num = numValue;
+            if (num < 0) {numValue = num * -1;}
+            strValue = new String(new char[(int) numValue]).replace("\0", other.getStrValue());
+           
+            return new Value(strValue);
+        }
+        return new Value();
     }
 
     @Override
