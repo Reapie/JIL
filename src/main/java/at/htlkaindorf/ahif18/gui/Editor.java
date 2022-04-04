bf5df7ee4ad2cab44268d5a5252977bfa8363f1e
Rluffy <fabi.ladenhaufen@gmail.com>
2022-04-04 15:31:18 +0200

    GUI, LineNumbers Hinzugefügt

--- src/main/java/at/htlkaindorf/ahif18/gui/Editor.java
+++ src/main/java/at/htlkaindorf/ahif18/gui/Editor.java
@@ -4,10 +4,12 @@ import at.htlkaindorf.ahif18.eval.Evaluator;
 import com.formdev.flatlaf.FlatIntelliJLaf;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Element;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class Editor {
 
@@ -16,6 +18,8 @@ public class Editor {
     private JButton btnFile;
     private JButton RUN;
     private JTextArea codeArea;
+    private JScrollPane jsp;
+    private  JTextArea lines;
 
     public Editor() {
         RUN.addActionListener(actionEvent -> run());
@@ -28,6 +32,44 @@ public class Editor {
         });
         codeArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 14));
         console.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 14));
+        lines = new JTextArea("1");
+        lines.setBackground(Color.LIGHT_GRAY);
+        lines.setEditable(false);
+        lines.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 14));
+
+
+        codeArea.getDocument().addDocumentListener(new DocumentListener() {
+            public String getText(){
+                int caretPosition = codeArea.getDocument().getLength();
+                Element root = codeArea.getDocument().getDefaultRootElement();
+                String text = "1" + System.getProperty("line.separator");
+
+                for(int i = 2; i < root.getElementIndex(caretPosition) + 2; i++) {
+                    text += i + System.getProperty("line.separator");
+                }
+
+                return text;
+            }
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                    lines.setText(getText());
+
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                lines.setText(getText());
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                lines.setText(getText());
+            }
+        });
+
+
+        jsp.setRowHeaderView(lines);
+
     }
 
     public static void main(String[] args) {
@@ -38,10 +80,13 @@ public class Editor {
         frame.setSize(512, 512);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
     }
 
     public void run () {
         String output = Evaluator.pipeline(codeArea.getText());
         console.setText(output);
     }
+
+
 }
